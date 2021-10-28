4d4ca2abbe237ff21ae649f21e231cb2e85ccc7c
Benjamin_Ajisefinni <benfinnirr@gmail.com>
2021-10-27 16:46:40 -0800

    Created Day 1 Python Project Band Name Generator

---
+++ day01/devOps/BandNameGenerator.py
@@ -0,0 +1,11 @@
+"""
+The Band Name Generator creates a unique name
+from user inputs
+"""
+
+# Greeting
+print('Welcome to the Band Name Generator\n-----------')
+city_name = input('What is the name of the city your grew up in?: \n')
+pet_name = input('What is the name of your favourite Pet? : \n')
+
+print('Your Unique Band Name is: \n------------\n' + city_name + ' ' + pet_name + '.')
