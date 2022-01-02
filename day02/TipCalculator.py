dbc9d18ffb8ad74bad8423b742b0795cdd24572e
Benjamin_Ajisefinni <benfinnirr@gmail.com>
2022-01-02 13:34:48 -0900

    Project Tip Calculartor completed

--- day02/TipCalculator.py
+++ day02/TipCalculator.py
@@ -4,3 +4,29 @@
 # Author: AJ
 #############################################
 
+print("Welcome to the Tip Calculator")
+print()
+
+# Get user input needed to calculate tip
+bill = input("What was the total Bill?: ")
+tip_percentage = input("What percentage would you like "
+                       "to Tip 10, 12, or 15? :")
+bill_sharing = input("How many people are sharing the Bill? :")
+
+# Convert string to number for calculation
+bill_input = float(bill)
+tip_input = int(tip_percentage)
+bill_sharing_input = int(bill_sharing)
+
+# Forumla for calculating Tip
+tip_calculated = bill_input * (100 + tip_input) / 100
+
+# Total to tip based on those sharing
+total_to_tip = round (tip_calculated, 2) / bill_sharing_input
+
+print()
+print(f"Total Bill: ${bill_input}")
+print(f"Amount to tip: {tip_input}%")
+print(f"Total Splitting Bill: {bill_sharing_input}")
+print(f"Each person should pay: ${total_to_tip}")
+
